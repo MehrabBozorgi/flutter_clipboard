ee80b30120b369d128662699e6fff513d625fc0a
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2023-01-23 11:23:54 +0330

    flutter_clipboard

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton.icon(
+            onPressed: () {
+              Clipboard.setData(
+                const ClipboardData(text: 'somte text'),
+              );
+            },
+            icon: const Icon(Icons.copy),
+            label: const Text(
+              'save to clipboard',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
